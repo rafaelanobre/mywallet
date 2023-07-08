e0ccdd26e3c5e6fa9ac4a3c4b5415663318c0562
rafaelanobre <raafanobre@gmail.com>
2023-07-07 21:04:41 -0300

    Fix: sign up and sign in functions

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,55 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useContext, useState } from "react";
+import { BASEURL } from "../constants/urls";
+import axios from "axios";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  function signin(e){
+    e.preventDefault();
+    setLoading(true);
+
+    axios.post(`${BASEURL}/`,{email, senha: password} )
+    .then(resp =>{
+      console.log(resp.data);
+      setLoading(false);
+      navigate(`/home`);
+    })
+    .catch(error =>{
+      if(error.response.status === 422){
+        alert("O e-mail informado é inválido.");
+        setLoading(false);
+        return;
+      }
+      if(error.response.status === 404){
+        alert("O e-mail informado não está cadastrado, cadastre-se primeiro.");
+        setLoading(false);
+        return;
+      }
+      if(error.response.status === 401){
+        alert("Senha incorreta");
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+    })
+  }
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signin}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input required placeholder="E-mail" type="email" onChange={(e) => setEmail(e.target.value)} disabled={loading} />
+        <input required placeholder="Senha" type="password" autocomplete="new-password" onChange={(e) => setPassword(e.target.value)} disabled={loading} />
+        <button type="submit" disabled={loading}>Entrar</button>
       </form>
 
-      <Link>
+      <Link to='/cadastro'>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,59 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import { BASEURL } from "../constants/urls";
+import axios from "axios";
 
 export default function SignUpPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function signup (e){
+    e.preventDefault();
+    setLoading(true);
+
+    if(password !== confirmPassword){
+      alert("As senhas não são iguais");
+      setLoading(false);
+      return;
+    }
+  
+    axios.post(`${BASEURL}/cadastro`, { name, email, password })
+    .then(resp =>{
+      setLoading(false);
+      navigate(`/`)})
+    .catch(error =>{
+      if(error.response.status === 422){
+        alert("O e-mail informado deve ser válido e a senha deve ter no mínimo 3 caracteres.");
+        setLoading(false);
+        return;
+      }
+      if(error.response.status === 409){
+        alert("E-mail já vinculado a outro usuário, faça login.");
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+    })
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signup}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input required placeholder="Nome" type="text" onChange={(e) => setName(e.target.value)} disabled={loading} />
+        <input required placeholder="E-mail" type="email" onChange={(e) => setEmail(e.target.value)} disabled={loading} />
+        <input required placeholder="Senha" type="password" autocomplete="new-password" onChange={(e) => setPassword(e.target.value)} disabled={loading} />
+        <input required placeholder="Confirme a senha" type="password" autocomplete="new-password" onChange={(e) => setConfirmPassword(e.target.value)} disabled={loading} />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to='/'>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
@@ -27,4 +66,8 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  input .error {
+    border: 1px solid rgb(255 0 0);
+  }
 `
